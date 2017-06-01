976c7319b4b6e34d3833ec4ae94e8941965cab77
Simon Ever-Hale <severhal@oberlin.edu>
2017-06-01 16:07:24 +0200

    commented code a little

--- scripts/main.js
+++ scripts/main.js
@@ -15,15 +15,14 @@ var database = firebase.database();
 
 var input = document.getElementById("input-crop");
 var output = document.getElementById("output-crop");
-
 var datalist = document.getElementById("crops");
 
 var companionRef = database.ref("/");
-var companions;
-companionRef.once('value', function(snapshot) {
+var companions; // this will hold all info about companion crops
+companionRef.once('value', function(snapshot) { // runs once with value of data at companionRef held in snapshot
   companions = snapshot.val();
-  Object.keys(companions).forEach(function (item) {
-    var option = document.createElement('option');
+  Object.keys(companions).forEach(function (item) { // for each crop
+    var option = document.createElement('option'); // add to datalist so that we can have autocomplete suggestions for user
     option.value = item;
     datalist.appendChild(option);
   });
@@ -32,9 +31,9 @@ companionRef.once('value', function(snapshot) {
     var val = "";
     var crops = companions[input.value.toLowerCase()];
     if (crops != null) {
-      var goodList = crops["good"];
+      var goodList = crops["good"]; // object containing all complementary crops as its keys
       if (goodList != null) {
-        val = Object.keys(goodList).join('</br>');
+        val = Object.keys(goodList).join('</br>'); // kind of a hack, maybe should use an unordered list instead
       }
       else {
         val = "No companion crops found"
@@ -44,10 +43,9 @@ companionRef.once('value', function(snapshot) {
       val = "Crop not found";
     }
     output.innerHTML = val;
-    console.log(val);
   };
 
-  input.oninput();
+  input.oninput(); // we run once so it handles the input already in the box ("Apple")
 });
 
 }
