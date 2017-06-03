fcb59f5cdf5ca4a7df240338f4c23a5fb1f7dc84
Simon Ever-Hale <severhal@oberlin.edu>
2017-06-03 13:58:31 +0200

    testing flex box, added bad column

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,7 @@ var database = firebase.database();
 
 var input = document.getElementById("input-crop");
 var output = document.getElementById("output-crop");
+var badOutput = document.getElementById("output-crop-bad");
 var datalist = document.getElementById("crops");
 
 var companionRef = database.ref("/");
@@ -29,6 +30,7 @@ companionRef.once('value', function(snapshot) { // runs once with value of data 
 
   input.oninput = function() {
     var val = "";
+    var badVal = "";
     var crops = companions[input.value.toLowerCase()];
     if (crops != null) {
       var goodList = crops["good"]; // object containing all complementary crops as its keys
@@ -36,13 +38,23 @@ companionRef.once('value', function(snapshot) { // runs once with value of data 
         val = Object.keys(goodList).join('</br>'); // kind of a hack, maybe should use an unordered list instead
       }
       else {
-        val = "No companion crops found"
+        val = "No companion crops found";
+      }
+
+      var badList = crops["bad"];
+      if (badList != null) {
+        badVal = Object.keys(badList).join('</br>');
+      }
+      else {
+        badVal = "No bad companion crops found";
       }
     }
     else {
       val = "Crop not found";
+      badVal = val;
     }
     output.innerHTML = val;
+    badOutput.innerHTML = badVal;
   };
 
   input.oninput(); // we run once so it handles the input already in the box ("Apple")
